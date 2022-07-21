9003ec7eefc6497471809c60f4f5ca825638c871
Sergey Nagornykh <serega1905@ukr.net>
2022-07-21 17:19:48 +0300

    Add tailwindcss, postcss, autoprefixer

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <header>
     <img
       alt="Vue logo"
